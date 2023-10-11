7dc9b60134b34ba562e033cfa739e0b7aa1c25c6
rameeskannantavided <0ramees0@gmail.com>
2023-10-11 22:16:34 +0400

    added

---
+++ movieproject/movieapp/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.contrib import admin
+from django.urls import path, include
+app_name = 'movieapp'
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('movie/<int:movie_id>',views.details,name='details'),
+    path('add/',views.add_movie,name='add_movie'),
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update')
+]
